ed28a7f31ca52071bf8d662bb3e73eb772f1f144
BuildTools <unconfigured@null.spigotmc.org>
2022-06-27 13:18:06 -0700

    changed to absolute imports

--- spooky/__init__.py
+++ spooky/__init__.py
@@ -1,16 +1,16 @@
 __version__ = '1.0.0'
 
-from .fits import fits_to_np
-from .fits import read_two_column
-from .fits import get_image_data
-from .Spec import Spec
-from .Spec import MultiSpec
-from .Spec import readfits
-from .Spec import read
-from .tools import redshift
-from .tools import to_air
-from .tools import get_continuum_points
-#from .tools import equivalent_width_error
-from .tools import equivalent_width
+from spooky.fits import fits_to_np
+from spooky.fits import read_two_column
+from spooky.fits import get_image_data
+from spooky.Spec import Spec
+from spooky.Spec import MultiSpec
+from spooky.Spec import readfits
+from spooky.Spec import read
+from spooky.tools import redshift
+from spooky.tools import to_air
+from spooky.tools import get_continuum_points
+#from spooky.tools import equivalent_width_error
+from spooky.tools import equivalent_width
 
-print('Loading Specplot')
+print('Loading spooky')
