2ed1e63c96dd22da27219d3e905ed3cd2b57b42f
Ted Johnson <tedjohnson12@g.ucla.edu>
2022-07-07 09:16:50 -0700

    added redshift tests

--- tests/tools_tests.py
+++ tests/tools_tests.py
@@ -7,3 +7,15 @@ def test_redshift():
 
     #speed of light blueshift
     assert 0 == tools.redshift(4000*u.AA,-c.c)
+    
+    #no radial velocity
+    l = 4000*u.AA
+    assert l == tools.redshift(l,0*u.km/u.s)
+
+    #Ca K
+    l = 3933.667 * u.AA
+    assert 3934.0213 * u.AA == pytest.approx(tools.redshift(l,27*u.km/u.s),abs=1e-3*u.AA)
+
+    # array
+    l = np.array(1100,1200)*u.AA
+    assert (l == tools.redshift(l,0*u.km/s)).all()
