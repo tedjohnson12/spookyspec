699b6145be270685876311fe1558e4df7b8015b8
Ted Johnson <tedjohnson12@g.ucla.edu>
2022-07-06 19:52:15 -0700

    Added docstrings

--- spooky/tools.py
+++ spooky/tools.py
@@ -196,13 +196,49 @@ def convert_line(x,y,ycont,lam):
     """
     return x-lam,(y-ycont)*-1
 
-def equivalent_width(x,y,ycont,w1,w2,unit,**kwargs):
-    """
-    Get the EW of a region
-    only unit passed as Quantity
-    """
+def calc_snr(x,y,ycont,w1,w2):
+    """Calculate SNR
+
+    Calculate the signal-to-noise ratio of a spectrum.
+
+    Args:
+        x (np.array): wavelength values of the spectrum
+        y (np.array): flux values of the spectrum
+        ycont (np.array): flux values of the continuum
+        w1 (float): starting wavelength with same units as `x`
+        w2 (float): ending wavelength with same units as `x`
     
-    plot = kwargs.get('plot',False)
+    Returns:
+        (float): the SNR of the spectrum
+    
+    """
+    is_cont = get_continuum_points(x,y)
+
+    res = np.abs(ycont[is_cont]-y[is_cont])
+
+    return (y[is_cont]/res).mean()
+
+
+
+def equivalent_width(x,y,ycont,w1,w2,unit,plot=False):
+    """Equivalent width
+
+    Measure the equivalent width of a region of the spectrum
+
+    Args:
+        x (np.array): wavelength values of the spectrum
+        y (np.array): flux values of the spectrum
+        ycont (np.array): flux values of the continuum
+        w1 (float): starting wavelength
+        w2 (float): ending wavelength
+        unit (astropy.units.Unit): units of `x`, `w1`, and `w2`
+
+    Keyword Args:
+        plot (bool): whether or not to plot the spectrum
+
+    Returns:
+        (astropy.Quantity): equivalent width of region
+    """
     
     reg = (x >= w1) & (x <= w2)
     xx = x[reg]
@@ -233,101 +269,32 @@ def equivalent_width(x,y,ycont,w1,w2,unit,**kwargs):
     return ew*unit
 
 
-def equivalent_width_error(x,y,W1,W2,linecenter,unit,**kwargs):
-    """
-    estimate the error of the equivalent width of a region of the spectrum
-    use with caution
-    """
+def equivalent_width_error(x,y,ycont,W1,W2,unit):
+    """Equivalent width error
+
+    Estimate the uncertainty in the equivalent width of a region
     
-    plot = kwargs.get('plot',False)
-    
+    Args:
+        x (np.array): wavelength values of the spectrum
+        y (np.array): flux values of the spectrum
+        ycont (np.array): flux values of the continuum
+        W1 (float): starting wavelength
+        W2 (float): ending wavelength
+        unit (astropy.units.Unit): units of `x`, `W1`, and `W2`
+
+
+    Returns:
+        (astropy.Quantity): equivalent width uncertainty of region
+    """
+        
     reg = (x>W1) & (x < W2)
     xx = x[reg]
     yy = y[reg]
-    is_cont = get_continuum_points(xx,yy,**kwargs)
-    
-    # reduce to linecenter region
-    indicies = []
-    for i in range(1,len(is_cont)):
-        if is_cont[i-1] != is_cont[i]:
-            indicies.append(i)
-    indicies = np.array(indicies)
-    
-    line_index = np.argwhere((xx-linecenter) > 0 )[0][0]
-    i1 = indicies[indicies < line_index][-1]
-    i2=indicies[indicies > line_index][0]
-    is_cont[:] = True
-    is_cont[i1:i2] = False
-    
-    #now compute error
-    df = np.std(yy[is_cont])
-    lx=xx[np.logical_not(is_cont)]
-    ly=yy[np.logical_not(is_cont)]
-    
-    error = trap_rule(lx,df/ly)
-    
-    if plot:
-        fig, ax = plt.subplots(1,1)
-        ax.plot(xx[is_cont],yy[is_cont],c='k')
-        ax.plot(lx,ly, c='b')
-        ax.set_xlim(W1,W2)
-        ymin = min(yy) * 0.9
-        ymax = max(yy) * 1.1
-        ax.set_ylim(ymin,ymax)
-        ax.axvline(linecenter,c='k',alpha=0.5)
-        ax.set_xlabel('Lambda (' + str(unit) + ')')
-        ax.set_ylabel('Flux')
-        ax.set_title('EW_error = %.4f ' % error + str(unit))
-        fig.show()
-        plt.pause(2)
-        plt.close(fig)
-    
-    return error*unit
+    yycont = ycont[reg]
+    snr = calc_snr(xx,yy,yycont)
 
+    yy_err = np.abs(snr/yycont)
 
-def Voigt(x,linecenter,AG,sigma,AL,gamma):
-    """
-    Pseudo-Voigt function
-    
-    x: array like
-    linecenter: center of profile
-    AG: Gaussian amplitude
-    sigma: gaussian sigma
-    AL: Lorentz amplitude
-    gamma: Lorentzian gamma
-    
-    Note: The Lorentzian portion of the curve is not normalized for purposes of easier fitting using scipy.optimize.curve_fit.
-    The area under the curve it therefore int(-inf-->inf) Voigt dx = AG + AL*pi*gamma
-    
-    """
-    y = (G_amp*(1/(G_sig*(np.sqrt(2*np.pi))))*(np.exp(-((x-linecenter)**2)/(2*(G_sig)**2)))) +((L_amp*L_wid**2/((x-linecenter)**2+L_wid**2)))
-    return y
+    ew_err = np.sqrt(trap_rule(xx,yy_err**2))
 
-def multiVoigt(x,*args):
-    """
-    use with caution
-    """
-    if len(args) % 5 != 0:
-        raise ValueError('must have 5 arguments for each profile')
-    nlines = int(len(args)/5)
-    y = np.zeros(len(x))
-    for i in range(nlines):
-        y = y + Voigt(x,args[i],args[i+1],args[i+2],args[i+3],args[i+4])
-    return y
-
-
-class Voigt_Profile:
-    """
-    use with caution
-    """
-    def __init__(self,popt,pconv=(0,0,0,0,0)):
-        self.center = (popt[0],pconv[0])
-        self.AG = (popt[1],pconv[1])
-        self.sigma = (popt[2],pconv[2])
-        self.AL = (popt[3],pconv[3])
-        self.gamma = (popt[4],pconv[4])
-    def Vofx(self,x):
-        return Voigt(x,self.center[0],self.AG[0],self.sigma[0],self.AL[0],self.gamma[0])
-    def area(self):
-        return self.AG + self.AL*np.pi*self.gamma
-    
+    return ew_err * unit
