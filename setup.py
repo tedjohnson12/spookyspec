8fd3503d7185ccac4b9eaf89242f90095e98e95b
Ted Johnson <tedjohnson12@g.ucla.edu>
2022-09-25 17:52:25 -0400

    metadata

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 setup(
-name='spookyspec',
-version='1.0.0',
-packages=find_packages()
+    name='spookyspec',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/tedjohnson12/spookyspec',
+    author='Ted Johnson',
+    author_email='tedjohnson12@g.ucla.edu',
+    license='GPL-3.0'
 )
